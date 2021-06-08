df240d80ff34512a359c9ae9a7cf9ca48d8e3897
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-06-08 16:10:27 +0530

    Initialise

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// Components
+
+// Dependencies
+
+// Pretty stuff
+import "./assets/styles/index.scss";
+
+const App = () => {
+	return (
+		<>
+			<header className="App-header">Hello World</header>
+		</>
+	);
+};
+
+export default App;
